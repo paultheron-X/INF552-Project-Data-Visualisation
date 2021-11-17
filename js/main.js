f4ac94f907e4c558a34a1817c5dc64f0c407f739
Jérémie Dentan <jeremie.dentan@live.com>
2021-11-17 11:59:02 +0100

    Jérémie

--- js/main.js
+++ js/main.js
@@ -96,71 +96,6 @@ var addCountries = function(){
             });
 };
 
-var fadeWaterIn = function(){
-    var path4proj = d3.geoPath()
-                      .projection(ctx.currentProj);
-    // clipping
-    var defs = d3.select("svg").insert("defs", "#map");
-    defs.append("path")
-        .datum({type: "Sphere"})
-        .attr("id", "sphere")
-        .attr("d", path4proj);
-    defs.append("path")
-        .datum({type: "Sphere"})
-        .attr("id", "clipSphere")
-        .attr("d", path4proj);
-    defs.append("clipPath")
-        .attr("id", "clip")
-        .append("use")
-        .attr("xlink:href", "#clipSphere");
-    d3.select("svg")
-        .insert("use", "#map")
-        .attr("class", "sphereBounds")
-        .attr("opacity", 0)
-        .attr("xlink:href", "#sphere")
-        .transition()
-        .duration(ctx.TRANSITION_DURATION)
-        .attr("opacity", 1);
-    // rivers and lakes
-    var riverG = ctx.mapG.append("g").attr("id", "rivers").attr("opacity", 0);
-    riverG.selectAll("path.rivers")
-          .data(ctx.rivers.features)
-          .enter()
-          .append("path")
-          .attr("d", path4proj)
-          .attr("class", "river");
-    var lakeG = ctx.mapG.append("g").attr("id", "lakes").attr("opacity", 0);
-    lakeG.selectAll("path.lakes")
-        .data(ctx.lakes.features)
-        .enter()
-        .append("path")
-        .attr("d", path4proj)
-        .attr("class", "lake");
-    riverG.transition().duration(ctx.TRANSITION_DURATION).attr("opacity", 1);
-    lakeG.transition().duration(ctx.TRANSITION_DURATION).attr("opacity", 1);
-};
-
-var fadeWaterOutBeforeProjSwitch = function(sourceProj, targetProj){
-    d3.select("g#rivers")
-      .transition()
-      .duration(ctx.TRANSITION_DURATION)
-      .attr("opacity", 0)
-      .on("end", function(d){d3.select(this).remove();});
-    d3.select("g#lakes")
-      .transition()
-      .duration(ctx.TRANSITION_DURATION)
-      .attr("opacity", 0)
-      .on("end", function(d){d3.select(this).remove();});
-    d3.select(".sphereBounds")
-      .transition()
-      .duration(ctx.TRANSITION_DURATION)
-      .attr("opacity", 0)
-      .on("end", function(){
-          d3.selectAll("defs").remove();
-          d3.selectAll("use").remove();
-          animateProjection(sourceProj, targetProj);
-      });
-};
 
 var getGlobalView = function(){
     d3.select("#map")
@@ -169,20 +104,6 @@ var getGlobalView = function(){
       .attr("transform", "translate(0,0) scale(1,1)");
 };
 
-var switchProjection = function(toER){
-    // toER = true => enabling pan-zoom => moving to EquiRectangular proj
-    // toER = false => disabling pan-zoom => moving to Interrupted Mollweide proj
-    if (toER){
-        ctx.currentProj = PROJECTIONS.ER;
-        d3.select("#pz").call(d3.zoom().transform, d3.zoomIdentity);
-        fadeWaterOutBeforeProjSwitch(PROJECTIONS.IM, PROJECTIONS.ER);
-    }
-    else {
-        // toIM
-        ctx.currentProj = PROJECTIONS.IM;
-        fadeWaterOutBeforeProjSwitch(PROJECTIONS.ER, PROJECTIONS.IM);
-    }
-}
 
 var animateProjection = function(sourceProj, targetProj){
     var transCount = 0;
@@ -196,29 +117,6 @@ var animateProjection = function(sourceProj, targetProj){
       });
 };
 
-var projectionTween = function(sourceProj, targetProj){
-    return function(d) {
-        var t = 0;
-        // fix for water offset in anim
-        var h_offset = (sourceProj == PROJECTIONS.ER) ? 8 : 0;
-        var projection = d3.geoProjection(project)
-                           .scale(1)
-                           .translate([MAP_W / 2, MAP_H / 2 + h_offset]);
-        var path = d3.geoPath()
-                     .projection(projection);
-        function project(λ, φ) {
-            λ *= 180 / Math.PI;
-            φ *= 180 / Math.PI;
-            var p0 = sourceProj([λ, φ]);
-            var p1 = targetProj([λ, φ]);
-            return [(1 - t) * p0[0] + t * p1[0], (1 - t) * -p0[1] + t * -p1[1]];
-        }
-        return function(_) {
-            t = _;
-            return path(d);
-        };
-    };
-}
 
 var createViz = function(){
     console.log("Using D3 v"+d3.version);
@@ -232,21 +130,10 @@ var createViz = function(){
 };
 
 var loadData = function(svgEl){
-    var promises = [d3.json("ne_50m_admin_0_countries.geojson"),
-                    d3.json("ne_50m_rivers_lake_centerlines.geojson"),
-                    d3.json("ne_50m_lakes.geojson"),
-                    d3.csv("drinking_water.csv")];
-    // var promises = [d3.json("ne_110m_admin_0_countries.geojson"),
-    //                 d3.json("ne_110m_rivers_lake_centerlines.geojson"),
-    //                 d3.json("ne_110m_lakes.geojson"),
-    //                 d3.csv("drinking_water.csv")];
+    var promises = [d3.json("data/carte_json/a-dep2021.json")];
+    
     Promise.all(promises).then(function(data){
-        restructureData(data[0], data[1], data[2], data[3]);
-        makeMap(svgEl);
+        console.log(data[0])
+        
     }).catch(function(error){console.log(error)});
-};
-
-var togglePZMode = function(){
-    ctx.panZoomMode = !ctx.panZoomMode;
-    switchProjection(ctx.panZoomMode);
 };
